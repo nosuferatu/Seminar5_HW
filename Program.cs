3e4368935686436058040389f5c19dcbb43027a3
nosuferatu <kuznetsovpf@gmail.com>
2023-04-26 23:22:08 +0300

    add task2 solution

--- Program.cs
+++ Program.cs
@@ -37,3 +37,44 @@ void PrintArray(int[] numbers)
     Console.Write("]");
     Console.WriteLine();
 }
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// Например:
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.Clear();
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Заданный массив: ");
+PrintArray(numbers);
+
+int sum = 0;
+
+for (int t = 1; t < numbers.Length; t+=2)
+    sum = sum + numbers[t];
+
+    Console.WriteLine($"Сумма элементов на нечетных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(-100,100);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
